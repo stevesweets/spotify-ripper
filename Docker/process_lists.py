3f7a2f2af0f7c26d6ad2acd90ec50b9285447acb
whit3he4d <whit3@gmx.com>
2020-06-10 09:23:24 +0200

    Update process_lists.py
    skipping already done lists

--- Docker/process_lists.py
+++ Docker/process_lists.py
@@ -7,6 +7,10 @@ lists_dir = "/root/.spotify-ripper/share/lists"
 download_lists = os.listdir(lists_dir)
 
 for curr_list in download_lists:
-    print("\n\n\n[ + ]  P R O C E S S I N G  C U R R E N T  L I S T :{}\n\n\n".format(curr_list))
-    subprocess.call('spotify-ripper {}'.format(curr_list), shell=True, cwd=lists_dir)
-    subprocess.call('mv {} {}.done'.format(curr_list,curr_list), shell=True, cwd=lists_dir)
+    if curr_list.rfind(".done") is not -1:
+        print("\n\n\n[ + ]  P R O C E S S I N G  C U R R E N T  L I S T :{}\n\n\n".format(curr_list))
+        subprocess.call('spotify-ripper {}'.format(curr_list), shell=True, cwd=lists_dir)
+        subprocess.call('mv {} {}.done'.format(curr_list,curr_list), shell=True, cwd=lists_dir)
+    else:
+        print("\n\n\n[ - ]  S K I P P I N G  P R O C E S S E D  L I S T :{}\n\n\n".format(curr_list))
+
