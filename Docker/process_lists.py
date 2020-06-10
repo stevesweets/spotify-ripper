88afd6468df634d5c599e4fa173e6f95cfd1e342
whit3he4d <whit3@gmx.com>
2020-06-10 17:09:29 +0200

    fix
    excluding folders from filelist

--- Docker/process_lists.py
+++ Docker/process_lists.py
@@ -3,14 +3,14 @@
 import sys, os, subprocess
 
 lists_dir = "/root/.spotify-ripper/share/lists"
+download_lists = []
 
-download_lists = os.listdir(lists_dir)
+for fname in os.listdir(lists_dir):
+    absolute_fname = os.path.join(lists_dir, fname)
+    if not os.path.isdir(absolute_fname) :
+        download_lists.append(fname)
 
 for curr_list in download_lists:
-    if curr_list.rfind(".done") is not -1:
-        print("\n\n\n[ + ]  P R O C E S S I N G  C U R R E N T  L I S T :{}\n\n\n".format(curr_list))
-        subprocess.call('spotify-ripper {}'.format(curr_list), shell=True, cwd=lists_dir)
-        subprocess.call('mv {} {}.done'.format(curr_list,curr_list), shell=True, cwd=lists_dir)
-    else:
-        print("\n\n\n[ - ]  S K I P P I N G  P R O C E S S E D  L I S T :{}\n\n\n".format(curr_list))
+    print("\n\n\n[ + ]  P R O C E S S I N G  C U R R E N T  L I S T :{}\n\n\n".format(curr_list))
+    subprocess.call('spotify-ripper {} && mv {} done/{}'.format(curr_list,curr_list,curr_list), shell=True, cwd=lists_dir)
 
